d32f7981a68fd2d1c6f77f6808698f36ecb8e2b6
Asher Glick <asher.glick@gmail.com>
2012-10-02 12:30:15 -0400

    cleaned up copy to clipboard

--- pipein.h
+++ pipein.h
@@ -9,50 +9,29 @@
 int copyToClipboard(std::string contents)
 {
     int fd1[2];
-    //int fd2[2];
     pid_t pid;
     // Create a pipe
-    //if ( (pipe(fd1) < 0) || (pipe(fd2) < 0) ) return -2; // Pipe Error (parent)
     if ( (pipe(fd1) < 0) ) return -2; // Pipe Error (parent)
     // fork into a new process
     if ( (pid = fork()) < 0 ) return -3; // Fork Error (parent)
+    
     // Child process
     else  if (pid == 0) {
         close(fd1[1]);
-        //close(fd2[0]);
-
         if (fd1[0] != STDIN_FILENO) {
-            if (dup2(fd1[0], STDIN_FILENO) != STDIN_FILENO) {
-                std::cerr << "CHILD: dup2 error to stdin" << std::endl;
-            }
+            if (dup2(fd1[0], STDIN_FILENO) != STDIN_FILENO) { return -5; // Dup2 error (Child)
             close(fd1[0]);
         }
-
-        /*
-        if (fd2[1] != STDOUT_FILENO) {
-            if (dup2(fd2[1], STDOUT_FILENO) != STDOUT_FILENO) {
-                std::cerr << "CHILD: dup2 error to stdout" << std::endl;
-            }
-            close(fd2[1]);
-        }*/
-        
         char *args[] = { (char*)"xclip" , (char*)"-sel", (char*)"clip", (char*)0};
-        if ( execv("/usr/bin/xclip", args) < 0) {
-            std::cerr << "CHILD: system error" << std::endl;
-            return -4;
-        }
+        if ( execv("/usr/bin/xclip", args) < 0) return -4; //execv error (CHILD)
         return 0;
     }
+
     // PARENT process
     else {
         close(fd1[0]);
-        //close(fd2[1]);
-        //contents += char(0);
-        if ( write(fd1[1], contents.c_str(), contents.size() ) != (int)contents.size() ) {
-            std::cerr << "PARENT: READ ERROR FROM PIPE" << std::endl;
-        }
+        if ( write(fd1[1], contents.c_str(), contents.size() ) != (int)contents.size() ) return -6; // WRITE ERROR FROM PIPE (PARENT)
         close(fd1[1]);
-        //close(fd2[0]);
         return 0;
     }
     return 0;
